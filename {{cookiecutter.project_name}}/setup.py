f672904fc8a491b5bccb103bd2f141f288fc1d94
jbloom <jbloom@fredhutch.org>
2019-04-23 20:34:20 -0700

    further organized jbloomlab template

---
+++ {{cookiecutter.project_name}}/setup.py
@@ -0,0 +1,52 @@
+"""Setup script for ``{{ cookiecutter.project_name }}``.
+
+Written by Jesse Bloom.
+"""
+
+import re
+from setuptools import setup
+import sys
+
+if not (sys.version_info[0] == 3 and sys.version_info[1] >= 6):
+    raise RuntimeError(
+                '{{ cookiecutter.project_name }} requires Python 3.6 or higher.\n'
+                'You are using Python {0}.{1}'.format(
+                sys.version_info[0], sys.version_info[1])
+                )
+
+# get metadata from package `__init__.py` file as here:
+# https://packaging.python.org/guides/single-sourcing-package-version/
+metadata = {}
+init_file = '{{ cookiecutter.project_name }}/__init__.py'
+with open(init_file) as f:
+    init_text = f.read()
+for dataname in ['version', 'author', 'email', 'url']:
+    matches = re.findall(
+            '__' + dataname + r'__\s+=\s+[\'"]([^\'"]+)[\'"]',
+            init_text)
+    if len(matches) != 1:
+        raise ValueError(f"found {len(matches)} matches for {dataname} "
+                         f"in {init_file}")
+    else:
+        metadata[dataname] = matches[0]
+
+with open('README.rst') as f:
+    readme = f.read()
+
+# main setup command
+setup(
+    name='{{ cookiecutter.project_name }}',
+    version=metadata['version'],
+    author=metadata['author'],
+    author_email=metadata['email'],
+    url=metadata['url'],
+    download_url='https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.project_name }}/tarball/' +
+                 metadata['version'],  # tagged version on GitHub
+    description='{{ cookiecutter.short_description }}',
+    long_description=readme,
+    license='GPLv3',
+    install_requires=[],
+    platforms='Linux and Mac OS X.',
+    packages=['{{ cookiecutter.project_name }}'],
+    package_dir={'{{ cookiecutter.project_name }}': '{{ cookiecutter.project_name }}'},
+)
---
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/__init__.py
@@ -0,0 +1,11 @@
+"""
+================================
+{{ cookiecutter.project_name }}
+================================
+
+"""
+
+__author__ = '{{ cookiecutter.full_name }}'
+__email__ = '{{ cookiecutter.email }}'
+__version__ = '{{ cookiecutter.version }}'
+__url__ = 'https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.project_name }}
